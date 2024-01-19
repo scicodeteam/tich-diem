db821adcfbefb8554a071c1ce5c0c96f67075a7f
quangphe <minhquang1356@gmail.com>
2024-01-19 11:52:46 +0700

    update gifts

--- frontend/css/Gifts/js/gifts_dh_1_0_0.js
+++ frontend/css/Gifts/js/gifts_dh_1_0_0.js
@@ -28,30 +28,33 @@ const gifts_dh_1_0_0__popup = `
     </section>
 `;
 
-// const gifts_dh_1_0_0__popupForm = `
-//     <div class="gifts_dh_1_0_0__overlay" id="gifts_dh_1_0_0__overlay"></div>
-//     <section class="gifts_dh_1_0_0__popupForm">
-//         <b id="close-form">╳</b>
-//         <div class="gifts_dh_1_0_0__popupTitle">
-//             Kiểm tra thông tin
-//         </div>
-//         <div class="gifts_dh_1_0_0__itemInput">
-//             <input type="text" placeholder="Họ và tên">
-//         </div>
-//         <div class="gifts_dh_1_0_0__itemInput">
-//             <input type="text" placeholder="Email">
-//         </div>
-//         <div class="gifts_dh_1_0_0__itemInput">
-//             <input type="text" placeholder="Số điện thoại">
-//         </div>
-//         <div class="gifts_dh_1_0_0__itemInput">
-//             <input type="text" placeholder="Địa chỉ">
-//         </div>
-//         <div class="gifts_dh_1_0_0__popupBtnForm">
-//             Xác nhận đổi thông tin
-//         </div>
-//     </section>
-// `;
+const gifts_dh_1_0_0__popupForm = `
+    <div class="gifts_dh_1_0_0__overlay" id="gifts_dh_1_0_0__overlay"></div>
+    <section class="gifts_dh_1_0_0__popupForm">
+        <b id="close-form">╳</b>
+        <div class="gifts_dh_1_0_0__btnBack">
+            ← Quay lại
+        </div>
+        <div class="gifts_dh_1_0_0__popupTitle">
+            Kiểm tra thông tin
+        </div>
+        <div class="gifts_dh_1_0_0__itemInput">
+            <input type="text" placeholder="Họ và tên">
+        </div>
+        <div class="gifts_dh_1_0_0__itemInput">
+            <input type="text" placeholder="Email">
+        </div>
+        <div class="gifts_dh_1_0_0__itemInput">
+            <input type="text" placeholder="Số điện thoại">
+        </div>
+        <div class="gifts_dh_1_0_0__itemInput">
+            <input type="text" placeholder="Địa chỉ">
+        </div>
+        <div class="gifts_dh_1_0_0__popupBtnForm">
+            Xác nhận đổi thông tin
+        </div>
+    </section>
+`;
 
 const gifts_dh = document.querySelectorAll(".knprice");
 for (let i = 0; i < gifts_dh.length; i++) {
@@ -74,24 +77,24 @@ for (let i = 0; i < gifts_dh.length; i++) {
         }
     });
 }
-// const giftsForm_dh = document.querySelectorAll(".kttt");
-// for (let i = 0; i < gifts_dh.length; i++) {
-//     giftsForm_dh[i].addEventListener('click', () => {
-//         document.getElementsByTagName('body')[0].insertAdjacentHTML("beforeend", gifts_dh_1_0_0__popupForm);
-//         document.getElementById('close-form').addEventListener('click', () => {
-//             gifts_dh_1_0_0__closeForm();
-//         });
-//         document.getElementById('gifts_dh_1_0_0__overlay').addEventListener('click', () => {
-//             gifts_dh_1_0_0__closeForm();
-//         });
-//         window.onclick = function (e) {
-//             if (e.target == document.querySelector('.gifts_dh_1_0_0__popupForm')) {
-//                 gifts_dh_1_0_0__closeForm();
-//             }
-//         };
-//         function gifts_dh_1_0_0__closeForm(){
-//             document.getElementById('gifts_dh_1_0_0__overlay').remove();
-//             document.querySelector('.gifts_dh_1_0_0__popupForm').remove();
-//         }
-//     });
-// }
+const giftsForm_dh = document.querySelectorAll(".kttt");
+for (let i = 0; i < gifts_dh.length; i++) {
+    giftsForm_dh[i].addEventListener('click', () => {
+        document.getElementsByTagName('body')[0].insertAdjacentHTML("beforeend", gifts_dh_1_0_0__popupForm);
+        document.getElementById('close-form').addEventListener('click', () => {
+            gifts_dh_1_0_0__closeForm();
+        });
+        document.getElementById('gifts_dh_1_0_0__overlay').addEventListener('click', () => {
+            gifts_dh_1_0_0__closeForm();
+        });
+        window.onclick = function (e) {
+            if (e.target == document.querySelector('.gifts_dh_1_0_0__popupForm')) {
+                gifts_dh_1_0_0__closeForm();
+            }
+        };
+        function gifts_dh_1_0_0__closeForm(){
+            document.getElementById('gifts_dh_1_0_0__overlay').remove();
+            document.querySelector('.gifts_dh_1_0_0__popupForm').remove();
+        }
+    });
+}
