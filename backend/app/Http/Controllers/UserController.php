08920ec23edd827812d08d7d588a6fd23ac3723a
huudinh <dinhndvn@yahoo.com>
2024-02-29 10:32:46 +0700

    update api

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,7 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth; 
 use Validator;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
+
 class UserController extends Controller 
+
 {
 public $successStatus = 200;
     /** 
@@ -58,4 +61,65 @@ public $successStatus = 200;
         $user = Auth::user(); 
         return response()->json(['success' => $user], $this-> successStatus); 
     } 
+ 
+    public function update(Request $request)
+    {
+        try {
+            $userCode = $request->query('ma_khach_hang');
+            $input = $request->all();
+            $user = User::where('ma_khach_hang',$userCode)->first();
+            if ($user === null || !$user) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'User not found',
+                ], 400);
+            
+            }
+            $user->update($input);
+            
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'data' => $user
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e
+            ], 500);
+        }
+    }
+    public function resetPassword(Request $request)
+    {
+        try {
+            $validator = FacadesValidator::make($request->all(), [
+                'id' => 'required',
+                'password' => 'required',
+                'c_password' => 'required|same:password',
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['status' => false, 'error' => $validator->errors()], 401);
+            }
+            $input = $request->all();
+            $user = User::find($input['id']);
+            if (!$user) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'User not found',
+                ], 400);
+            }
+            $input['password'] = bcrypt($input['password']);
+            $user->update($input);
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'data' => $user
+            ], 201);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => $e
+            ], 500);
+        }
+    }
 }
